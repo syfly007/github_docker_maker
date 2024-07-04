7ba6fca170beacd9f46529373df6eb2e92ef05b6
SunYuFei <syfly007@163.com>
2024-07-04 15:58:16 +0800

    fix

--- DEV/ftps/start.sh
+++ DEV/ftps/start.sh
@@ -4,6 +4,7 @@
 sed -i "s/\${FTP_PORT}/${FTP_PORT}/g" /etc/vsftpd/vsftpd.conf
 sed -i "s/\${PASV_MIN_PORT}/${PASV_MIN_PORT}/g" /etc/vsftpd/vsftpd.conf
 sed -i "s/\${PASV_MAX_PORT}/${PASV_MAX_PORT}/g" /etc/vsftpd/vsftpd.conf
+sed -i "s/\${PASV_IP}/${PASV_IP}/g" /etc/vsftpd/vsftpd.conf
 
 # 添加FTP用户
 if ! id -u "$FTP_USER" > /dev/null 2>&1; then
