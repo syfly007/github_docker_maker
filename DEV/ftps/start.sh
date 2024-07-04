4fd90dc12c0a656e1cfebb7d1dcc722a95f47e97
SunYuFei <syfly007@163.com>
2024-07-04 10:19:08 +0800

    add ftps

---
+++ DEV/ftps/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 修改vsftpd配置文件中的端口
+sed -i "s/\${FTP_PORT}/${FTP_PORT}/g" /etc/vsftpd.conf
+sed -i "s/\${PASV_MIN_PORT}/${PASV_MIN_PORT}/g" /etc/vsftpd.conf
+sed -i "s/\${PASV_MAX_PORT}/${PASV_MAX_PORT}/g" /etc/vsftpd.conf
+
+# 添加FTP用户
+if ! id -u $FTP_USER > /dev/null 2>&1; then
+    useradd -m $FTP_USER
+    echo "$FTP_USER:$FTP_PASS" | chpasswd
+fi
+
+# 启动vsftpd
+/usr/sbin/vsftpd /etc/vsftpd.conf
